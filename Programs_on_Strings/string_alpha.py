e33db35722816f512ab51a618e38bf7dee45a820
khanirfan196 <mr.irfankhan196@gmail.com>
2020-06-08 15:28:07 +0530

    Added String Alpha program

---
+++ Programs_on_Strings/string_alpha.py
@@ -0,0 +1,34 @@
+# Program to manipulate alphabets of a string.
+# Demo of Lower, Upper, Capitalize, Title, and Swapcase functions.
+# Author : Irfan Khan Mohammed
+
+str1 = "this is a simple string with no capital letters"
+str2 = "THIS IS A SIMPLE STRING WITH NO LOWER LETTERS"
+title = "this is title of an essay"
+str3 = "first letter of first word is small"
+str4 = "IRFAN khan"
+
+print()
+print(str1)
+print("Upper case func:")
+print(str1.upper())
+
+print()
+print(str2)
+print(" lower case func:")
+print(str2.lower())
+
+print()
+print(title)
+print("Title func:")
+print(title.title())
+
+print()
+print(str3)
+print("Capitalize func:")
+print(str3.capitalize())
+
+print()
+print(str4)
+print("Swapcase func:")
+print(str4.swapcase())
