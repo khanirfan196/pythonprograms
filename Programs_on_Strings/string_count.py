df4c1841c515563cdcb744c278c21be47c5e7fac
khanirfan196 <mr.irfankhan196@gmail.com>
2020-06-09 15:59:27 +0530

    Added String counting characters program

---
+++ Programs_on_Strings/string_count.py
@@ -0,0 +1,13 @@
+# Program to demonstrate count() function on strings
+# Author : Irfan Khan Mohammed
+
+str = "Always be Motivated and keep up the good work"
+
+print(str)
+print("Count func: counting occurrence of 'o'")
+print(str.count('o'))
+
+print()
+print("Count func: counting blank spaces:")
+print(str.count(' '))
+
