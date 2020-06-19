cf68ddb6b42930dce61d0fb818db954f83794337
khanirfan196 <mr.irfankhan196@gmail.com>
2020-06-19 21:04:45 +0530

    Added program on stripping a string

---
+++ Programs_on_Strings/string_strip.py
@@ -0,0 +1,14 @@
+# Program to remove redudant white spaces in a string
+
+str = "   This string has whitespaces at both sides   "
+
+print(str)
+print()
+print("Strips removed at both the sides")
+print(str.strip())
+print()
+print("Strips removed at right side")
+print(str.rstrip())
+print()
+print("Strips removed at left side")
+print(str.lstrip())
