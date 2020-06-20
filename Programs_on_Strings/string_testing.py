10e70f18ca38b07e43f9c86bc2950bfc186258ea
khanirfan196 <mr.irfankhan196@gmail.com>
2020-06-20 23:17:34 +0530

    Added program on testing functions of string

---
+++ Programs_on_Strings/string_testing.py
@@ -0,0 +1,41 @@
+# Program to demonstrate testing functions on strings
+# Author : Irfan Khan Mohammed
+
+str = "abcdefghijklmnopqrstuvwxyz"
+
+print(str)
+print("isupper func:")
+print(str.isupper())
+print()
+
+print("islower func:")
+print(str.islower())
+print()
+
+print("isalpha func:")
+print(str.isalpha())
+print()
+
+print("isalnum func:")
+print(str.isalnum())
+print()
+
+print("isdigit func:")
+print(str.isdigit())
+print()
+
+print("istitle func:")
+print(str.istitle())
+print()
+
+print("isspace func:")
+print(str.isspace())
+print()
+
+print("startswith func:")
+print(str.startswith('a'))
+print()
+
+print("endswith func:")
+print(str.endswith('z'))
+print()
