2bdd841e1e81fa81797e42b75e09eda898eb8612
khanirfan196 <mr.irfankhan196@gmail.com>
2020-06-21 21:21:13 +0530

    Added Program to convert string to list

---
+++ Programs_on_Strings/string_converting_to_list.py
@@ -0,0 +1,17 @@
+# Program to demonstrate conversion of string into list.
+# Author : Irfan Khan Mohammed
+
+str = "1 2 3 4 5 6 7 8 9"
+
+# Splitting list as per blank space
+a = str.split(' ')
+
+# Prints entire list
+print(a)
+
+# Prints first element which is char
+print(a[0])
+
+# Prints 'type' for element
+print(type(a[0]))
+
